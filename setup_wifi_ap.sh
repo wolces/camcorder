52c0f6e2c0ab0623901ff60a37fd612b81cbe924
jonathan <jseclow@gmail.com>
2026-02-09 10:25:35 -0800

    add port forwarding

--- setup_wifi_ap.sh
+++ setup_wifi_ap.sh
@@ -13,6 +13,11 @@ ip link set wlan0 up
 # enable ip forwarding (in case we want to bridge to internet later)
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
+# add port forwarding: redirect port 80 to 8080 for web interface
+# this allows users to access http://camcorder.local without :8080
+iptables -t nat -F  # flush existing nat rules
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+
 # start dnsmasq
 dnsmasq -C /home/camcorder/dnsmasq.conf
 
@@ -20,3 +25,4 @@ dnsmasq -C /home/camcorder/dnsmasq.conf
 hostapd -B /home/camcorder/hostapd.conf
 
 echo "wifi access point ready: ssid=Camcorder, password=recording, ip=192.168.4.1"
+echo "web interface: http://camcorder.local or http://192.168.4.1"
